004f3871d7881eb2e07e063d389b82a80840f580
marlio <jitao.liu82@gmail.com>
2021-01-01 20:24:01 +0800

    updated recently.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="dashboard-container">
-    <a href="https://github.com/Heeexy/SpringBoot-Shiro-Vue" target="_blank"><img style="margin:40px" src="../assets/img/vuejs.png"></a>
+<!--  <div class="dashboard-container">-->
+<!--    <a href="https://github.com/Heeexy/SpringBoot-Shiro-Vue" target="_blank">-->
+<!--      <img style="margin:20px" src="../assets/img/vuejs.png" alt="dashboard">-->
+<!--    </a>-->
+<!--  </div>-->
+  <div>
+    Text: {{txtName}} <label><input :value="txtName" @click="inputClick"></label>
   </div>
 </template>
 
@@ -9,8 +14,20 @@ export default {
   name: 'Dashboard',
   data () {
     return {
-
+      txtName: 'aaabbbccc'
+    }
+  },
+  methods: {
+    inputClick () {
+      alert('Input clicked! ' + this.txtName)
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.dashboard-container {
+  text-align: center;
+  margin: 15.5% 0 auto;
+}
+</style>
